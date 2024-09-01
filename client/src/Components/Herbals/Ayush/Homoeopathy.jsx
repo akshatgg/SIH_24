3db6e5971375a37baf6bb070574572999edc029c
akshatgg <akshatg9636@gmail.com>
2024-09-01 17:19:26 +0530

    adding aos

--- client/src/Components/Herbals/Ayush/Homoeopathy.jsx
+++ client/src/Components/Herbals/Ayush/Homoeopathy.jsx
@@ -1,10 +1,20 @@
 import Items from "../Items.jsx";
 import test1 from "../../../assets/test1.png"
+import AOS from 'aos';
 
+import React, { useEffect } from 'react';
+import 'aos/dist/aos.css';  // Import the AOS styles
 const Homoeopathy = () => {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000, // Animation duration in milliseconds
+      once: true,     // Whether animation should happen only once
+    });
+  }, []);
     return (
+
       <div>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-10 mt-10">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-10 mt-10 "  data-aos="fade-down">
           <Items
             title="Sample Product"
             description="This is a description of the sample product."
--- client/src/Components/Herbals/Herbals.jsx
+++ client/src/Components/Herbals/Herbals.jsx
@@ -3,16 +3,28 @@ import Items from "./Items.jsx";
 import logo from "../../assets/logo.png";
 import test1 from "../../assets/test1.png"
 import Tabsd from "./Tabsd/Tabsd.jsx";
+import AOS from 'aos';
+
+import { useEffect } from 'react';
+import 'aos/dist/aos.css';  // Import the AOS styles
 function Herbals() {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000, // Animation duration in milliseconds
+      once: true,     // Whether animation should happen only once
+    });
+  }, []);
   return (
     <div className="h-[250vh]">
-      <div className="mt-10 flex items-start justify-center">
+      <div className="mt-10 flex flex-col items-center justify-center " data-aos="fade-up">
         <img 
           src={logo} 
           className="w-auto h-32 object-contain" 
           alt="Logo" 
         />
+      <h1 className=" text-2xl font-semibold flex items-start justify-center">Every herbs are verified</h1>
       </div>
+
       <h1 className="mt-20 text-5xl font-serif  font-bold flex items-start justify-center">Featured Products</h1>
    
 
