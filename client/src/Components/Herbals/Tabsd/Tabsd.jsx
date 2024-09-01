3308354362aab2d9e394184b56142a42d594639a
akshatgg <akshatg9636@gmail.com>
2024-09-01 14:22:50 +0530

    updating color of tabs

--- client/src/Components/Herbals/Tabsd/Tabsd.jsx
+++ client/src/Components/Herbals/Tabsd/Tabsd.jsx
@@ -16,18 +16,18 @@ export function Tabsd() {
       title: "Ayurveda",
       value: "product",
       content: (
-        <div className="w-full overflow-hidden relative rounded-2xl p-10 text-2xl md:text-5xl font-bold text-white bg-gray-700">
+        <div className="w-full overflow-hidden relative rounded-2xl p-10 text-2xl md:text-5xl font-bold text-white bg-gray-400">
           <p>Ayurveda</p>
           <Ayurveda />
         </div>
       ),
-      style: activeTab === "product" ? "bg-gray-700 text-black" : "bg-gray-700 text-white",
+      style: activeTab === "product" ? "bg-gray-400 text-black" : "bg-gray-700 text-white",
     },
     {
       title: "Yoga & Naturopathy",
       value: "services",
       content: (
-        <div className="w-full overflow-hidden relative rounded-2xl p-10 text-2xl md:text-5xl font-bold text-white bg-gray-700">
+        <div className="w-full overflow-hidden relative rounded-2xl p-10 text-2xl md:text-5xl font-bold text-white bg-gray-400">
           <p>Yoga & Naturopathy</p>
           <Naturopathy />
         </div>
@@ -38,7 +38,7 @@ export function Tabsd() {
       title: "Unani",
       value: "playground",
       content: (
-        <div className="w-full overflow-hidden relative rounded-2xl p-10 text-2xl md:text-5xl font-bold text-white bg-gray-700">
+        <div className="w-full overflow-hidden relative rounded-2xl p-10 text-2xl md:text-5xl font-bold text-white bg-gray-400">
           <p>Unani</p>
           <Unani />
         </div>
@@ -49,7 +49,7 @@ export function Tabsd() {
       title: "Siddha",
       value: "content",
       content: (
-        <div className="w-full overflow-hidden relative rounded-2xl p-10 text-2xl md:text-5xl font-bold text-white bg-gray-700">
+        <div className="w-full overflow-hidden relative rounded-2xl p-10 text-2xl md:text-5xl font-bold text-white bg-gray-400">
           <p>Siddha</p>
           <Siddha />
         </div>
@@ -57,11 +57,11 @@ export function Tabsd() {
       style: "bg-gray-700 text-white",
     },
     {
-      title: "Homoeopathy",
+      title: "Sowa Rigpa & Homoeopathy",
       value: "random",
       content: (
-        <div className="w-full overflow-hidden relative rounded-2xl p-10 text-2xl md:text-5xl font-bold text-white bg-gray-700">
-          <p>Homoeopathy</p>
+        <div className="w-full overflow-hidden relative rounded-2xl p-10 text-2xl md:text-5xl font-bold text-white bg-gray-400">
+          <p>Sowa Rigpa & Homoeopathy</p>
           <Homoeopathy />
         </div>
       ),
@@ -70,10 +70,10 @@ export function Tabsd() {
   ];
 
   return (
-    <div className="w-full md:w-[100rem] [perspective:1000px] relative b flex flex-col mx-auto items-start justify-start my-40">
+    <div className="w-full md:w-[100rem] [perspective:1000px] relative b flex flex-col mx-auto items-center justify-center my-40 bg-gray-400 mb-7 rounded-full p-4 ">
       <Tabs 
         tabs={tabs}
-        tabClassName={`px-8 py-4 text-lg md:text-2xl font-semibold ${tabs.find(tab => tab.value === activeTab)?.style || "bg-gray-700 text-white"}`}
+        tabClassName={`px-8 py-4 ml-5 text-lg md:text-2xl flex item-center justify-center font-semibold ${tabs.find(tab => tab.value === activeTab)?.style || "bg-gray-700 text-white"}`}
         onTabChange={(newValue) => setActiveTab(newValue)}
       />
     </div>
