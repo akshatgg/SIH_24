96f437dff8852268429ccef1586facb368adfd66
akshatgg <akshatg9636@gmail.com>
2024-09-01 18:59:25 +0530

    adding down arrow btn and also in working condn

--- client/src/Components/Home/Home.jsx
+++ client/src/Components/Home/Home.jsx
@@ -1,11 +1,18 @@
-import herbal from "../../assets/herbal4.jpg";
+import React from "react";
+import { FaChevronDown } from "react-icons/fa6";
 import "./Home.css";
+import { Link } from 'react-scroll';
+
 
 function Home() {
   return (
     <div className="home-container">
-         
-       
+      <div className="arrow-container arrow-upper " name="Home">
+         <Link to="Herbals" spy={true} smooth={true} duration={250}>
+        <FaChevronDown size={24} color="#fff" /> 
+        </Link>
+      </div>
+   
     </div>
   );
 }
